06ecf81ade6722d83d6c025f47056c0b2ca4a674
DelxHQ <delxhq0@gmail.com>
2016-01-31 22:50:29 +0000

    Update Installer.php

--- src/pocketmine/wizard/Installer.php
+++ src/pocketmine/wizard/Installer.php
@@ -9,10 +9,10 @@ use pocketmine\utils\Config;
 use pocketmine\utils\Utils;
 
 class Installer{
-	const DEFAULT_NAME = "A BukkitPE: Server";
+	const DEFAULT_NAME = "A BukkitPE Server";
 	const DEFAULT_PORT = 19132;
 	const DEFAULT_MEMORY = 256;
-	const DEFAULT_PLAYERS = 20;
+	const DEFAULT_PLAYERS = 30;
 	const DEFAULT_GAMEMODE = 0;
 
 	private $lang;
