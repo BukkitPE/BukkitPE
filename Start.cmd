a419091606491b3c7ad9961585d9adb342f64057
Andre <andrethegamer911@gmail.com>
2016-02-03 12:46:26 -0500

    Update Start.cmd

--- Start.cmd
+++ Start.cmd
@@ -22,7 +22,7 @@ if exist BukkitPE.phar (
 )
 
 REM if exist bin\php\php_wxwidgets.dll (
-REM 	%PHP_BINARY% %POCKETMINE_FILE% --enable-gui %*
+REM 	%PHP_BINARY% %BukkitPE_FILE% --enable-gui %*
 REM ) else (
 	if exist bin\mintty.exe (
 		start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="DejaVu Sans Mono" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "BukkitPE server software" -i bin/pocketmine.ico -w max %PHP_BINARY% %BukkitPE_FILE% --enable-ansi %*
