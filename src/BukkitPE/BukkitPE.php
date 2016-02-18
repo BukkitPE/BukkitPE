a284b33a3ed2c154a38ecc860ef0633133103dbb
Andre <andrethegamer911@gmail.com>
2016-02-18 10:03:00 -0500

    Updated to support 0.14 MCPE Versions

--- src/BukkitPE/BukkitPE.php
+++ src/BukkitPE/BukkitPE.php
@@ -53,10 +53,10 @@ namespace BukkitPE {
 	use BukkitPE\wizard\Installer;
 
 	const VERSION = "1.0dev";
-	const API_VERSION = "1.13.2";
-	const CODENAME = "Stable_V1";
-	const MINECRAFT_VERSION = "v0.13.2 alpha";
-	const MINECRAFT_VERSION_NETWORK = "0.13.2";
+	const API_VERSION = "0.14.0";
+	const CODENAME = "Stable_V2";
+	const MINECRAFT_VERSION = "v0.14.0 alpha";
+	const MINECRAFT_VERSION_NETWORK = "0.14.0";
 
 	/*
 	 * Startup code. Do not look at it, it may harm you.
