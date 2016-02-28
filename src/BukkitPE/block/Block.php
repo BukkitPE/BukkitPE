537ba508ab973a1c386b1e271b736330d5b280a9
Dylan <delxhq0@gmail.com>
2016-02-28 21:51:11 +0000

    Comment!

--- src/BukkitPE/block/Block.php
+++ src/BukkitPE/block/Block.php
@@ -1,4 +1,25 @@
 <?php
+/*
+ *
+ * ____        _    _    _ _   _____  ______ 
+ * |  _ \      | |  | |  (_) | |  __ \|  ____|
+ * | |_) |_   _| | _| | ___| |_| |__) | |__   
+ * |  _ <| | | | |/ / |/ / | __|  ___/|  __|  
+ * | |_) | |_| |   <|   <| | |_| |    | |____ 
+ * |____/ \__,_|_|\_\_|\_\_|\__|_|    |______|
+ *                                           
+ *                                          
+ *
+ * This program is free software, and it's under the  Creative Commons 
+ * Attribution-NonCommercial-NoDerivatives 4.0 , You can modify. But 
+ * you may not use the material for commercial purposes. 
+ *
+ * @author BukkitPE Team
+ * @link http://www.bukkitpe.net/
+ *
+ *
+*/
+
 /**
  * All Block classes are in here
  */
