3494662fb19aa8b65797274c1029c5da889551ec
Andre <andrethegamer911@gmail.com>
2016-02-29 09:36:13 -0500

    Changed installer license

--- src/BukkitPE/wizard/Installer.php
+++ src/BukkitPE/wizard/Installer.php
@@ -59,10 +59,8 @@ class Installer{
 		echo $this->lang->welcome_to_BukkitPE . "\n";
 		echo <<<LICENSE
 
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU Lesser General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+  BukkitPE is run under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0. 
+  
 
 LICENSE;
 		echo "\n[?] " . $this->lang->accept_license . " (y/N): ";
