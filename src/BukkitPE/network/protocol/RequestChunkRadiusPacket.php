d8c58f7e0cbe9dbc87e80cc8cfbddb7804487498
Andre <andrethegamer911@gmail.com>
2016-04-21 12:33:54 -0400

    That's it i think..

---
+++ src/BukkitPE/network/protocol/RequestChunkRadiusPacket.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ *
+ * ____        _    _    _ _   _____  ______ 
+ * |  _ \      | |  | |  (_) | |  __ \|  ____|
+ * | |_) |_   _| | _| | ___| |_| |__) | |__   
+ * |  _ <| | | | |/ / |/ / | __|  ___/|  __|  
+ * | |_) | |_| |   <|   <| | |_| |    | |____ 
+ * |____/ \__,_|_|\_\_|\_\_|\__|_|    |______|
+ *                                           
+ *                                          
+ *
+ * This program is free software, and it's under the  Creative Commons 
+ * Attribution-NonCommercial-NoDerivatives 4.0 , You can modify. But 
+ * you may not use the material for commercial purposes. 
+ *
+ * @author BukkitPE Team
+ * @link http://www.bukkitpe.net/
+ *
+ *
+*/
+
+namespace BukkitPE\network\protocol;
+
+
+
+class RequestChunkRadiusPacket extends DataPacket{
+	const NETWORK_ID = Info::REQUEST_CHUNK_RADIUS_PACKET;
+
+	public $radius;
+
+	public function decode(){
+		$this->radius = $this->getInt();
+	}
+
+	public function encode(){
+	}
+
+}
