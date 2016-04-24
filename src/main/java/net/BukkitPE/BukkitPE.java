587f610da527d0182c262521fbbc3481df7e4019
Andre <andrethegamer911@gmail.com>
2016-04-23 21:39:23 -0400

    Basic starting things

---
+++ src/main/java/net/BukkitPE/BukkitPE.java
@@ -0,0 +1,31 @@
+/*
+ *
+ * ____        _    _    _ _   _____  ______ 
+ * |  _ \      | |  | |  (_) | |  __ \|  ____|
+ * | |_) |_   _| | _| | ___| |_| |__) | |__   
+ * |  _ <| | | | |/ / |/ / | __|  ___/|  __|  
+ * | |_) | |_| |   <|   <| | |_| |    | |____ 
+ * |____/ \__,_|_|\_\_|\_\_|\__|_|    |______|
+ *                                           
+ *                                          
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author BukkitPE Team
+ * @link http://www.bukkitpe.net/
+ *
+ *
+*/
+package net.BukkitPE;
+
+import net.BukkitPE.command.CommandReader; 
+import net.BukkitPE.utils.MainLogger;
+import net.BukkitPE.utils.ServerKiller;
+
+public class BukkitPE {
+
+ public final static String VERSION = "1.0.0";
+    public final static String API_VERSION = "1.0.0";
