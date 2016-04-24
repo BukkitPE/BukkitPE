d3a3d62a205495349fa0f53bab74f19d22977ea5
Andre <andrethegamer911@gmail.com>
2016-04-23 22:03:16 -0400

    Tiles, thread killing.ect) Added

--- src/main/java/net/BukkitPE/BukkitPE.java
+++ src/main/java/net/BukkitPE/BukkitPE.java
@@ -21,11 +21,89 @@
 */
 package net.BukkitPE;
 
-import net.BukkitPE.command.CommandReader; 
+import net.BukkitPE.command.CommandReader;  
 import net.BukkitPE.utils.MainLogger;
 import net.BukkitPE.utils.ServerKiller;
 
 public class BukkitPE {
 
  public final static String VERSION = "1.0.0";
-    public final static String API_VERSION = "1.0.0";
+ public final static String API_VERSION = "1.0.0";
+ public final static String CODENAME = "Cake";
+ public final static String MINECRAFT_VERSION = "v0.14.1 alpha";
+ public final static String MINECRAFT_VERSION_NETWORK = "0.14.1";
+
+ public final static String PATH = System.getProperty("user.dir") + "/";
+ public final static String DATA_PATH = System.getProperty("user.dir") + "/";
+ public final static String PLUGIN_PATH = DATA_PATH + "plugins";
+ public final static String CRASH_DUMP = DATA_PATH + "Dumps"; // Stores server crashdumps..ect) 
+ public static final long START_TIME = System.currentTimeMillis();
+ public static boolean ANSI = true;
+ public static boolean shortTitle = false;
+ public static int DEBUG = 1;
+ public static void main(String[] args) {
+
+        // Shorten the tiles if needed for old Win8 builds ()
+        String osName = System.getProperty("os.name").toLowerCase();
+        if (osName.contains("windows")) {
+            if (osName.contains("windows 8") || osName.contains("2012")) {
+                shortTitle = true;
+            }
+        }
+        // For linux based os
+       String osName = System.getProperty("os.name").toLowerCase();
+        if (osName.contains("linux")) {
+            if (osName.contains("Linux") || osName.contains("2012")) {
+                shortTitle = false;
+            }
+    }
+        //Starting parameters
+        for (String arg : args) {
+            switch (arg) {
+                case "disable-ansi":
+                    ANSI = false;
+                    break;
+            }
+        }
+
+        MainLogger logger = new MainLogger(DATA_PATH + "server.log");
+
+        try {
+            if (ANSI) {
+                System.out.print((char) 0x1b + "]0;Starting BukkitPE Server For Minecraft: PE" + (char) 0x07);
+            }
+            Server server = new Server(logger, PATH, DATA_PATH, PLUGIN_PATH);
+        } catch (Exception e) {
+            logger.logException(e);
+        }
+
+        if (ANSI) {
+            System.out.print((char) 0x1b + "]0;Stopping Server standby..." + (char) 0x07);
+        }
+        logger.info("Stopping other threads");
+
+        for (Thread thread : java.lang.Thread.getAllStackTraces().keySet()) {
+            if (!(thread instanceof InterruptibleThread)) {
+                continue;
+            }
+            logger.debug("Stopping " + thread.getClass().getSimpleName() + " thread");
+            if (thread.isAlive()) {
+                thread.interrupt();
+            }
+        }
+
+        ServerKiller killer = new ServerKiller(8);
+        killer.start();
+
+        logger.shutdown();
+        logger.interrupt();
+        CommandReader.getInstance().removePromptLine();
+
+        if (ANSI) {
+            System.out.print((char) 0x1b + "]0;Server has stopped" + (char) 0x07);
+        }
+        System.exit(0);
+    }
+
+
+}
