276eeef79abaf3eaa2f2caf0b91c45db656a9490
Andre <andrethegamer911@gmail.com>
2016-06-09 12:51:31 -0400

    Create cb.java

---
+++ src/main/java/net/CraftBukkit/cb.java
@@ -0,0 +1,41 @@
+/*
+ *
+ * ____        _    _    _ _   _____  ______ 
+ * |  _ \      | |  | |  (_) | |  __ \|  ____|
+ * | |_) |_   _| | _| | ___| |_| |__) | |__   
+ * |  _ <| | | | |/ / |/ / | __|  ___/|  __|  
+ * | |_) | |_| |   <|   <| | |_| |    | |____ 
+ * |____/ \__,_|_|\_\_|\_\_|\__|_|    |______|
+ *                                           
+ *                                          
+ *
+ * This program is free software, and it's under GNU General Public License v3.0+
+ * You can redistribute it and/or modify under the same license.
+ *
+ * @author BukkitPE Team
+ * @link http://www.bukkitpe.net/
+ *
+ *
+*/
+
+package net.BukkitPE.CraftBukkit;
+
+import net.BukkitPE.Server;
+import net.BukkitPE.BukkitPE;
+import net.BukkitPE.Player
+
+/**
+ * Support for Bukkit plugin(S) and spigot which both is the same.  Doesn't matter.
+ */
+
+public final class bp {
+
+final boolean ScoreBoard = false;
+final boolean Tips = true;
+final boolean Popup = true
+int protocol = 70;
+
+}
+/**
+ * Alot of things needs to be re-defined
+ */
