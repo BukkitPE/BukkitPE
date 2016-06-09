50733560832f5c5d322280f3d0afd9770ec6038e
Andre <andrethegamer911@gmail.com>
2016-06-09 12:34:02 -0400

    That's it!

--- src/main/java/net/BukkitPE/Player.java
+++ src/main/java/net/BukkitPE/Player.java
@@ -1,3 +1,22 @@
+/*
+ *
+ * ____        _    _    _ _   _____  ______ 
+ * |  _ \      | |  | |  (_) | |  __ \|  ____|
+ * | |_) |_   _| | _| | ___| |_| |__) | |__   
+ * |  _ <| | | | |/ / |/ / | __|  ___/|  __|  
+ * | |_) | |_| |   <|   <| | |_| |    | |____ 
+ * |____/ \__,_|_|\_\_|\_\_|\__|_|    |______|
+ *                                           
+ *                                          
+ *
+ * This program is free software, and it's under GNU General Public License v3.0+
+ * You can redistribute it and/or modify under the same license.
+ *
+ * @author BukkitPE Team
+ * @link http://www.bukkitpe.net/
+ *
+ *
+*/
 package net.BukkitPE;
 
 import net.BukkitPE.block.Block;
