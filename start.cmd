0809f12ffa7a563903b81ca46e8d74d74c394faf
Danny Piper <djpiper28@users.noreply.github.com>
2016-08-26 21:43:12 +0100

    Added license to startup routine.

--- start.cmd
+++ start.cmd
@@ -1,2 +1,7 @@
+@echo off
+title BukkitPE
+echo This is licensed under GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007.
+echo See here for more details.https://github.com/BukkitPE/BukkitPE/blob/master/LICENSE
+echo Started
 java -jar BukkitPE.jar
 pause
