23eec63f9b633ae0d6331cf01e057b7482f52027
Danny Piper <djpiper28@users.noreply.github.com>
2016-08-26 21:46:21 +0100

    debugging deubug.java

--- src/main/java/net/BukkitPE/debug.java
+++ src/main/java/net/BukkitPE/debug.java
@@ -1,5 +1,7 @@
 package net.BukkitPE;
-public function debug(string string){
-  System.out.print((char) 0x1b + "]0;" + string + (char) 0x07);
+public class debug(){
+  function debug(string string){
+    System.out.print((char) 0x1b + "]0;" + string + (char) 0x07);
+  }
+  //call net.BukkitPE.debug("Text to output to console, to use; it will reduce the jar file size.");
 }
-//call net.BukkitPE.debug("Text to output to console, to use; it will reduce the jar file size.");
