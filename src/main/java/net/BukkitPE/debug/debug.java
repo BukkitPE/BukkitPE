7871b424ef9f176ae4d936eef62785bd43204f0b
Andre <andrethegamer911@gmail.com>
2016-08-27 01:24:35 -0400

    Revert
    Please guys, test before pushing! :+1:

--- src/main/java/net/BukkitPE/debug/debug.java
+++
@@ -1,8 +0,0 @@
-package net.BukkitPE.debug;
-public class debug{
-  function debug(string astring){
-	  string aastring = "]0;" + astring;
-    System.out.print((char) 0x1b + aastring + (char) 0x07);
-  }
-  //call net.BukkitPE.debug("Text to output to console, to use; it will reduce the jar file size.");
-}
