47a3cab12342d9e5b442d6f77f7726d8e472776a
Ankit <ankitmaharjankath@gmail.com>
2016-11-01 12:18:12 +0545

    Starting!!

--- start.cmd
+++ start.cmd
@@ -2,6 +2,6 @@
 title BukkitPE
 echo This is licensed under GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007.
 echo See here for more details.https://github.com/BukkitPE/BukkitPE/blob/master/LICENSE
-echo Started
+echo Starting BukkitPE
 java -jar BukkitPE.jar
 pause
